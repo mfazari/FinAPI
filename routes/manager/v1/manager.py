a84bfd5ccfa2754f27dca3497f0f05dfb172fb96
Massimo Fazari <massimofazari@massimos-mbp.home>
2022-11-07 20:41:53 +0000

    Add new route

--- routes/manager/v1/manager.py
+++ routes/manager/v1/manager.py
@@ -6,10 +6,10 @@ import yfinance as yf
 manager_page = Blueprint("manager_page", __name__, template_folder="templates")
 
 
-@manager_page.route("/v1/manager/search/<symbol>", methods=["GET"])
-def get_stock_by_symbol(symbol):
+@manager_page.route("/v1/manager/search/<search_name>", methods=["GET"])
+def get_organization_by_symbol(search_name):
     response = urllib.request.urlopen(
-        f"https://query2.finance.yahoo.com/v1/finance/search?q={symbol}"
+        f"https://query2.finance.yahoo.com/v1/finance/search?q={search_name}"
     )
     content = response.read()
     stock_data = json.loads(content)
@@ -24,3 +24,12 @@ def get_stock_by_symbol(symbol):
         # data[x]["symbol"] = stock_data["quotes"][x]["symbol"]
 
     return data
+
+
+@manager_page.route("/v1/manager/stock/<symbol>", methods=["GET"])
+def get_stock_by_symbol(symbol):
+    data = yf.download(tickers=symbol, period='5d', interval='15m', rounding=True)
+    print(data)
+    dict = data.to_json(orient='table')
+
+    return dict
