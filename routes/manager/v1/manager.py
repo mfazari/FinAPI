9149feb83a1162ea44d2ed69319d702a3c701204
Massimo Fazari <massimofazari@massimos-mbp.home>
2022-11-30 23:06:57 +0000

    Add Service Layer

--- routes/manager/v1/manager.py
+++ routes/manager/v1/manager.py
@@ -1,39 +1,21 @@
 import json
 import urllib
-from flask import Flask, Blueprint
 import yfinance as yf
+
+from flask import Flask, Blueprint
+from services.services import manager_service
+
 
 manager_page = Blueprint("manager_page", __name__, template_folder="templates")
 
 
 @manager_page.route("/v1/manager/search/<search_name>", methods=["GET"])
 def get_organization_by_symbol(search_name):
-    response = urllib.request.urlopen(
-        f"https://query2.finance.yahoo.com/v1/finance/search?q={search_name}"
-    )
-    content = response.read()
-    stock_data = json.loads(content)
-
-    data = []
-    for x in range(len(stock_data["quotes"])):
-        temp_dict = {
-            "id": x,
-            "name": stock_data["quotes"][x]["longname"],
-            "symbol": stock_data["quotes"][x]["symbol"],
-        }
-        data.append(temp_dict)
-        # data[x]["symbol"] = stock_data["quotes"][x]["symbol"]
-
+    data = manager_service.get_organization_by_symbol(search_name)
     return data
 
 
 @manager_page.route("/v1/manager/stock/<symbol>", methods=["GET"])
 def get_chart_data_by_symbol(symbol):
-    data = yf.download(tickers=symbol, period='3mo', interval='1d', rounding=True)
-    print(data)
-    dict = data.to_json(orient='table')
-    # dict["max_high"] = max(data.High)
-    # dict["min_high"] = min(data.High)
-    # print(dict)
-
-    return dict
+    data = manager_service.get_chart_data(symbol)
+    return data
---
+++ services/services.py
@@ -0,0 +1,32 @@
+import json
+import yfinance as yf
+import urllib
+
+
+class ManagerService:
+    def get_organization_by_symbol(self, search_name):
+        response = urllib.request.urlopen(
+            f"https://query2.finance.yahoo.com/v1/finance/search?q={search_name}"
+        )
+        content = response.read()
+        stock_data = json.loads(content)
+        data = []
+        for x in range(len(stock_data["quotes"])):
+            temp_dict = {
+                "id": x,
+                "name": stock_data["quotes"][x]["longname"],
+                "symbol": stock_data["quotes"][x]["symbol"],
+            }
+            data.append(temp_dict)
+
+            return data
+
+    def get_chart_data(self, symbol):
+        data_pandas = yf.download(tickers=symbol, period='3mo', interval='1d', rounding=True)
+        data_dict = json.loads(data_pandas.to_json(orient='table'))
+        data_dict["max_high"] = max(data_pandas.High)
+        data_dict["min_high"] = min(data_pandas.High)
+        return data_dict
+
+
+manager_service = ManagerService()
